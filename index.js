33ef61d50560453480592197b465e84c60d2aff0
Tenmayos <tenmayos_2@hotmail.com>
2023-01-02 00:56:17 +0300

    Added mongoose dependency and the necessary dependencies and setups for recieving data from the frontend properly (cors, multer)

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
+const upload = multer();
+const cors = require("cors");
 
 const app = express();
 const port = 4000;
 
+app.use(cors());
+//app.use(bodyParser.json());
 app.listen(port, () => {
     console.log("up and running on port: " + port);
+});
+
+
+
+app.post("/", upload.any(),(request, response) => {
+    console.log(request.files[0]);
+    response.sendStatus(201);
 })
