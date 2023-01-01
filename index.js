7dc71633c9c6b186b2ff504ce1804d570fa198d8
Tenmayos <tenmayos_2@hotmail.com>
2023-01-02 01:38:41 +0300

    -Connected to the database -successfully stored all the text data

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const upload = multer();
 const cors = require("cors");
-
+const upload = multer();
 const app = express();
 const port = 4000;
 
 app.use(cors());
-//app.use(bodyParser.json());
 app.listen(port, () => {
     console.log("up and running on port: " + port);
 });
+const dbName = "applicantsDB";
+const dbConnectionString = "mongodb://localhost:27017/" + dbName;
 
+mongoose.set("strictQuery", false);
+mongoose.connect(dbConnectionString);
 
+const applicantSchema = new mongoose.Schema({
+    fullName: String,
+    phone: String,
+    email: String,
+    school: String,
+    major: String,
+    gpa: String,
+    totalGpa: String,
+});
+
+const Applicant = mongoose.model("applicant", applicantSchema);
 
 app.post("/", upload.any(),(request, response) => {
-    console.log(request.files[0]);
+    
+    const textData = request.body;
+    console.log(textData);
+    const applic = new Applicant({
+        fullName: textData.fullName,
+        phone: textData.phone,
+        email: textData.email,
+        school: textData.school,
+        major: textData.major,
+        gpa: textData.major,
+        totalGpa: textData.totalGpa
+    });
+
+    applic.save();
     response.sendStatus(201);
-})
+});
