65abcb4ec310233c68452441d20319b9c239b5c5
Tenmayos <tenmayos_2@hotmail.com>
2023-01-10 17:29:48 +0300

    Added explanitory code comments

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const upload = multer();
 const app = express();
 const port = 4000;
 
+// Using cors to avoid webapp warnings from blocking the localhost connection when the front-end tries to interact with it, this is for development environment only.
 app.use(cors());
+
 app.listen(port, () => {
     console.log("up and running on port: " + port);
 });
+
+// Creating a database connection string on the localhost with mongodb's default ports.
 const dbName = "applicantsDB";
 const dbConnectionString = "mongodb://localhost:27017/" + dbName;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(dbConnectionString);
 
+// Creating a schema to shape the data that will be recieved from the front-end.
 const applicantSchema = new mongoose.Schema({
     fullName: String,
     phone: String,
@@ -35,8 +40,10 @@ const applicantSchema = new mongoose.Schema({
     }
 });
 
+// Setting the data model using the schema.
 const Applicant = mongoose.model("applicant", applicantSchema);
 
+// Creating the post route to store the data into the database.
 app.post("/", upload.any(),(request, response) => {
     
     const textData = request.body;
